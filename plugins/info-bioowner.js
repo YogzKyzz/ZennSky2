7a3a1c8573bf6a89984cf31107eb4404bc90c360
ZennSky <108292979+ZennSky@users.noreply.github.com>
2022-07-19 01:04:35 +0700

    Update info-bioowner.js

--- plugins/info-bioowner.js
+++ plugins/info-bioowner.js
@@ -2,23 +2,23 @@ let handler = async (m, { conn }) => {
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `*───────[ BIODATA OWNER ]───────*
 *💌 Nama* : YogzKyz
-*✉️ Nama RL* : Fahri
+*✉️ Nama RL* : Yogi
 *♂️ Gender* : Laki - laki
 *🕋 Agama* : Islam
-*⏰ Tanggal lahir* : 10 Juni 2007
-*🎨 Umur* : 15
-*🧮 Kelas* : 9
-*🧩 Hobby* : Nonton Hanime, Chatting, Recode script bot
-*💬 Sifat* : Idiot, Tidak Ramah, Kanjut, Prik, Pedofil
-*🗺️ Tinggal* : Indo, Riau, Indragiri Hulu, Air Molek
-*❤️ Suka* : warnah kuning & biru, Hanime, waifu, loli, trap, furry, kucing
-*💔 Benci* : autis, anak epep, seleb
+*⏰ Tanggal lahir* : 10 Maret 2005
+*🎨 Umur* : 17
+*🧮 Kelas* : 12
+*🧩 Hobby* : Turu, Coding, Recode Script Bot
+*💬 Sifat* : Kalem Kok Tenang Aja
+*🗺️ Tinggal* : Mars atau Pluto juga bisa
+*❤️ Suka* : Warna Hijau, Hitam, Putih and Kamauh😙
+*💔 Benci* : Autis and Seleb
 
 *───────[ SOSIAL MEDIA ]───────*
-*📷 instagran* : @bot_whangsaf
-*🇫  Facebook* : Adison Fahri
-*🏮 Chanel Youtube* : Papah-Chan
-*🐈 Github:* FahriAdison
+*📷 instagran* : @karfuanyogich
+*🎟️ TikTok* : @yogzkyzx
+*🏮 ChYoutube* : YogzKy
+*🐈 Github:* ZennSky
 
 `
 conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
