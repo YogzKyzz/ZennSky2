f0325438e3bd5e98795f6a2aa40e35af2fe55858
ZennSky <108292979+ZennSky@users.noreply.github.com>
2022-07-19 21:13:29 +0700

    Update _anti+212.js

--- plugins/_anti+212.js
+++ plugins/_anti+212.js
@@ -1,13 +1,13 @@
 let handler = m => m
 
 handler.before = async function (m) {
-   if (m.sender.startsWith('0' || '0')) {
+   if (m.sender.startsWith('212' || '212')) {
    	global.db.data.users[m.sender].banned = true
 conn.reply('see u next time')
 conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
    }
    
-   if (m.sender.startsWith('0' || '0')) {
+   if (m.sender.startsWith('212' || '212')) {
    	global.db.data.users[m.sender].banned = true
 conn.reply('see u next time')
 conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
