2b02b902c242933a8a33e5287acbe292f2a33c92
ZennSky <108292979+ZennSky@users.noreply.github.com>
2022-07-20 20:47:47 +0700

    Update general-menu2.js

--- plugins/general-menu2.js
+++ plugins/general-menu2.js
@@ -2,9 +2,9 @@ import moment from 'moment-timezone'
 let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
 let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
 let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
-conn.sendHydrated(m.chat, '*『 𝐈𝐍𝐅𝐎 𝐁𝐎𝐓 』*', `Halo, Aku Bot WhatsApp Dan Aku Diberi Nama ZennSky-Botz Oleh Ownerku<3.\n\nSilahkan klik tombol menu untuk Mengakses bot ini\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'https://youtube.com/channel/UCY8LFcKLPctR4SoeAH1DVSA', '𝐘𝐓 𝐎𝐖𝐍𝐄𝐑', null, null, [
+conn.sendHydrated(m.chat, '*『 𝐈𝐍𝐅𝐎 𝐁𝐎𝐓 』*', `Halo, Aku Bot WhatsApp Dan Aku Diberi Nama Araa-Botz Oleh Ownerku yang paling Cantik Nan Imut<3.\n\nSilahkan klik tombol MENU untuk Mengakses Bot Ini\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'https://youtube.com/channel/UCY8LFcKLPctR4SoeAH1DVSA', '𝐆𝐑𝐔𝐏 𝐁𝐎𝐓𝐙', null, null, [
 [`𝙼𝙴𝙽𝚄`, '.m'],
-[`\nSebenernya Gua Itu Tukang Ngocok Guys:)`, 'Bilek']
+[`\nAraa Cantik😙:)`, 'Bilek']
 ], m, {asLocation: true})
 }
 handler.help = ['menu']
